6250607d5e0d8cdfdd6ff3c6112c2dcb46e0de93
Emily Anderson <emily.anderson@stfc.ac.uk>
2022-11-02 12:38:13 +0000

    test patien_normalise with all zeros array

--- tests/test_models.py
+++ tests/test_models.py
@@ -62,6 +62,8 @@ def test_daily_min_string():
 @pytest.mark.parametrize(
     "test, expected",
     [
+        ([[0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0]]),
+        ([[1, 1, 1], [1, 1, 1], [1, 1, 1]], [[1, 1, 1], [1, 1, 1], [1, 1, 1]]),
         ([[1,2,3],[4,5,6],[7,8,9]], [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]])
     ]
 )
@@ -69,3 +71,4 @@ def test_patient_normalise(test, expected):
     """Test normlisations work"""
     from inflammation.models import patient_normalise
     npt.assert_almost_equal(patient_normalise(np.array(test)), np.array(expected), decimal=2)
+
